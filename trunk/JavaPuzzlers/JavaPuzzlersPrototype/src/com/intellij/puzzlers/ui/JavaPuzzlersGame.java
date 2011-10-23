bca616002da43e38257bbd2afcf6644e88382fda
fofanova.mn@gmail.com <fofanova.mn@gmail.com@e2796e2e-3198-963a-491a-b42dcee26890>
2011-10-23 21:47:13 +0000

    [JavaPuzzlersPrototype] Added answers.

--- trunk/JavaPuzzlers/JavaPuzzlersPrototype/src/com/intellij/puzzlers/ui/JavaPuzzlersGame.java
+++ trunk/JavaPuzzlers/JavaPuzzlersPrototype/src/com/intellij/puzzlers/ui/JavaPuzzlersGame.java
@@ -13,20 +13,49 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class JavaPuzzlersGame {
+    List<JRadioButton> answers = new ArrayList<JRadioButton>();
     private final Project project;
     private JButton runButton;
     private JPanel mainPanel;
     private JTextArea codeContent;
-    private JRadioButton answerRadioButton;
     private JButton nextPuzzlerButton;
     private JLabel javaCodeLabel;
+    private JRadioButton answer1;
+    private JRadioButton answer2;
+    private JRadioButton answer3;
+    private JLabel isRight;
+    private JButton answerButton;
     private int puzzlerNumber;
+    private int rightAnswer;
 
+    private void parseAnswers(int number) {
+        try {
+            File answerTemplateFile = getAnswersFile(number);
+            Scanner sc = new Scanner(answerTemplateFile);
+            answer1.setText(sc.nextLine());
+            answer2.setText(sc.nextLine());
+            answer3.setText(sc.nextLine());
+            rightAnswer = sc.nextInt();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
-    public void addPuzzler(int number) {
+    }
+
+    private boolean checkAnswer() {
+        if (answers.get(rightAnswer-1).isSelected()) {
+            isRight.setText("Your answer is right!");
+            return true;
+        } else {
+            isRight.setText("Right answer is #"+rightAnswer);
+            return false;
+        }
+    }
+
+    private void addPuzzler(int number) {
         try {
             File puzzlerTemplateFile = getPuzzleFile(number);
             Scanner sc = new Scanner(puzzlerTemplateFile);
@@ -38,10 +67,17 @@ public class JavaPuzzlersGame {
                 }
             }
             javaCodeLabel.setText("Puzzler number #" + number);
+            parseAnswers(number);
         } catch (FileNotFoundException e) {
             runButton.setEnabled(false);
             e.printStackTrace();
         }
+    }
+
+    private File getAnswersFile(int number) {
+        IdeaPluginDescriptor descriptor = PluginManager.getPlugin(PluginId.getId("com.jetbrains.puzzlers"));
+        String path = descriptor.getPath().getAbsolutePath() + File.separator;
+        return new File(path + "classes" + File.separator + "Answers" + number + ".in");
     }
 
     private File getPuzzleFile(int number) {
@@ -50,11 +86,28 @@ public class JavaPuzzlersGame {
         return new File(path + "classes" + File.separator + "Puzzler" + number + ".in");
     }
 
+    private void initAnswers() {
+        answers.clear();
+        answers.add(answer1);
+        answers.add(answer2);
+        answers.add(answer3);
+    }
+
     public JavaPuzzlersGame(Project project) {
         this.project = project;
         puzzlerNumber = 1;
+        initAnswers();
+        answerButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                checkAnswer();
+                runButton.setEnabled(true);
+                answerButton.setEnabled(false);
+            }
+        });
         nextPuzzlerButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                answerButton.setEnabled(true);
+                runButton.setEnabled(false);
                 puzzlerNumber++;
                 addPuzzler(puzzlerNumber);
             }
@@ -71,4 +124,5 @@ public class JavaPuzzlersGame {
     public JPanel getMainPanel() {
         return mainPanel;
     }
+
 }
