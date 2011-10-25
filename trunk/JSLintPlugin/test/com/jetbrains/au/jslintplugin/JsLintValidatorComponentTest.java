c1a04e86ef7dfcfb7547129c36dded95489e38ea
dmitriybandurin@gmail.com <dmitriybandurin@gmail.com@e2796e2e-3198-963a-491a-b42dcee26890>
2011-10-25 09:24:15 +0000

    right project structure

---
+++ trunk/JSLintPlugin/test/com/jetbrains/au/jslintplugin/JsLintValidatorComponentTest.java
@@ -0,0 +1,36 @@
+package com.jetbrains.au.jslintplugin;
+
+import org.junit.Test;
+
+/**
+ * User: Dmitriy Bandurin
+ * Date: 25.10.11
+ * Time: 12:54
+ */
+public class JsLintValidatorComponentTest {
+
+    @Test
+    public void testIsModified() throws Exception {
+
+    }
+
+    @Test
+    public void testApply() throws Exception {
+
+    }
+
+    @Test
+    public void testReset() throws Exception {
+
+    }
+
+    @Test
+    public void testGetState() throws Exception {
+
+    }
+
+    @Test
+    public void testLoadState() throws Exception {
+
+    }
+}
